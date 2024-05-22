a7c919f5b13102c30031b840714ad6998a3e06d2
EchoEmissary <pratyaac02@gmail.com>
2024-05-21 22:21:58 -0500

    SpringBoot: Initial Setup

---
+++ src/test/java/com/OnboardOrchestra/baseauthapp/BaseAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnboardOrchestra.baseauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
